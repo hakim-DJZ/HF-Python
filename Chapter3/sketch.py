67f6524d280ffbb72069d81cd636d724e9345bef
hakim-DJZ <hakim.sellaoui@gmail.com>
2016-05-14 16:56:40 +0100

    Latest update from home laptop.

--- Chapter3/sketch.py
+++ Chapter3/sketch.py
@@ -3,17 +3,17 @@ the one speaking and their speach."""
 
 import os
 
-os.chdir('C:/Users/hsellaoui/Documents/HF-Python/HF-Python/Chapter3')
+os.chdir('C:/Users/hakim/Documents/HF-Python/Chapter3/')
 
 data = open('sketch.txt')
 
 for eachLine in data:
-    if eachLine.find(':') > 0:
+    try:
         (role,line_spoken) = eachLine.split(":",1)
         print(role, end='')
         print(' said: ', end='')
         print(line_spoken, end='')
-    else:
-        print(eachLine, end='')        
+    except:
+        pass
 
 data.close()
