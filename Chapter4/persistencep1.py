9588fb41399291a7d08e390d6b4d36818e93fdd1
hakim-DJZ <hakim.sellaoui@gmail.com>
2016-05-15 22:36:31 +0100

    Forgot to stage latest work.

--- Chapter4/persistencep1.py
+++ Chapter4/persistencep1.py
@@ -2,6 +2,7 @@
 the one speaking and their speach."""
 
 import os
+import hakim_nester
 
 os.chdir('C:/Users/hakim/Documents/HF-Python/Chapter4/')
 
@@ -28,9 +29,9 @@ except IOError as err:
 
 try:
     with open('man_data.txt', "w") as man_out:
-        print(man, file=man_out)
+        hakim_nester.print_lol(man, 0,0, man_out)
     with open('other_data.txt', "w") as other_out: 
-        print(other, file=other_out)        
+        hakim_nester.print_lol(other,0,0, other_out)        
 
 except IOError as err:
     print("Couldn't open file" + str(err))
